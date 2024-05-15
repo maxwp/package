7f7b653367835de04bc8b68607324dac4cf997eb
Maxim Miroshnichenko <max@miroshnichenko.org>
2024-05-15 23:21:39 +0300

    math fix

--- Math/Math_ArrayVO.class.php
+++ Math/Math_ArrayVO.class.php
@@ -1,30 +1,24 @@
 <?php
 class Math_ArrayVO extends ArrayObject {
 
-    public function max($a) {
-        return Math_Array::Max($a);
+    public function max() {
+        return Math_Array::Max($this);
     }
 
-    public function min($a) {
-        return Math_Array::Min($a);
+    public function min() {
+        return Math_Array::Min($this);
     }
 
-    public function avg($a) {
-        return Math_Array::Avg($a);
+    public function avg() {
+        return Math_Array::Avg($this);
     }
 
-    public function sum($a) {
-        return Math_Array::Sum($a);
+    public function sum() {
+        return Math_Array::Sum($this);
     }
 
-    /**
-     * Calculate array median
-     *
-     * @param $a
-     * @return float
-     */
-    public function median($a, $countLimit = false) {
-        return Math_Array::Median($a, $countLimit);
+    public function median($countLimit = false) {
+        return Math_Array::Median($this, $countLimit);
     }
 
 }
