7b7102b46a759ed086fd9ef47a7fee1073ea6081
Maxim Miroshnichenko <max@miroshnichenko.org>
2025-01-09 12:00:39 +0200

    EE cli prints

--- EE/EE_AContentCli.class.php
+++ EE/EE_AContentCli.class.php
@@ -17,4 +17,25 @@ abstract class EE_AContentCli extends EE_AContent implements EE_IContent {
         }
     }
 
+    public function print_n($s = '') {
+        if (defined('EE_PRINT')) {
+            print $s.PHP_EOL;
+        }
+    }
+
+    public function print_t($s) {
+        if (defined('EE_PRINT')) {
+            print $s."\t";
+        }
+    }
+
+    public function print_r($a) {
+        if (defined('EE_PRINT')) {
+            print_r($a);
+        }
+    }
+
+    // @todo print_e (callback)
+    // @todo print_s (sprintf)
+
 }
