4c44d09736b97765f92d15e73962e434b74693c3
Maxim Miroshnichenko <max@miroshnichenko.org>
2025-07-28 01:03:35 +0300

    SL old vs new ws

--- StreamLoop/include.php
+++ StreamLoop/include.php
@@ -8,4 +8,5 @@ ClassLoader::Get()->registerClass(__DIR__.'/StreamLoop_UDP_DrainForward.class.ph
 ClassLoader::Get()->registerClass(__DIR__.'/StreamLoop_UDP_DrainBackward.class.php');
 ClassLoader::Get()->registerClass(__DIR__.'/StreamLoop_UDP_IReceiver.interface.php');
 ClassLoader::Get()->registerClass(__DIR__.'/StreamLoop_WebSocket.class.php');
+ClassLoader::Get()->registerClass(__DIR__.'/StreamLoop_WebSocketOld.class.php');
 ClassLoader::Get()->registerClass(__DIR__.'/StreamLoop_Timer.class.php');
