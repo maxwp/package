2c10bb0fa8c723a82ece99769e011781b9a289a5
Maxim Miroshnichenko <max@miroshnichenko.org>
2025-10-30 23:07:45 +0200

    SL_WS ext, SL_Timer ext

--- StreamLoop/StreamLoop_Timer.class.php
+++ StreamLoop/StreamLoop_Timer.class.php
@@ -2,55 +2,15 @@
 /**
  * @deprecated use ATimer
  */
-class StreamLoop_Timer extends StreamLoop_AHandler {
+class StreamLoop_Timer extends StreamLoop_ATimer {
 
-    public function __construct(StreamLoop $loop, $timerID, $timeout, StreamLoop_Timer_ICallback $callback) {
-        parent::__construct($loop);
-
-        $loop->unregisterHandler($this);
-
-        $this->setTimeout($timeout);
-
-        // @todo check id
-        $this->streamID = -1 * (int) $timerID; // id нужен отрицательный чтобы не пересекся с настоящими stream //
-        $this->stream = null;
-
-        $this->onCallback($callback);
-
-        $this->_loop->registerHandler($this);
-        $this->_loop->updateHandlerTimeoutTo($this, microtime(true) + $this->_timeout);
-    }
-
-    public function readyRead($tsSelect) {
-        // nothing
-    }
-
-    public function readyWrite($tsSelect) {
-        // nothing
-    }
-
-    public function readyExcept($tsSelect) {
-        // nothing
-    }
-
-    public function readySelectTimeout($tsSelect) {
+    protected function _onTimer($tsSelect) {
         $this->_callback->onTimer($this, $tsSelect);
-        $this->_loop->updateHandlerTimeoutTo($this, $tsSelect + $this->_timeout);
     }
 
-    public function onCallback(StreamLoop_Timer_ICallback $callback) {
+    public function setCallback(StreamLoop_Timer_ICallback $callback) {
         $this->_callback = $callback;
     }
-
-    public function setTimeout($timeout) {
-        $this->_timeout = (float) $timeout;
-    }
-
-    public function getTimeout() {
-        return $this->_timeout;
-    }
-
-    private $_timeout = 0.0;
 
     private StreamLoop_Timer_ICallback $_callback;
 
--- StreamLoop/StreamLoop_WebSocket_ICallback.interface.php
+++ StreamLoop/StreamLoop_WebSocket_ICallback.interface.php
@@ -6,5 +6,6 @@ interface StreamLoop_WebSocket_ICallback {
 
     public function onReceive(StreamLoop_WebSocket $handler, $tsSelect, $payload);
     public function onError(StreamLoop_WebSocket $handler, $tsSelect, $payload);
+    public function onReady(StreamLoop_WebSocket $handler, $tsSelect);
 
 }
